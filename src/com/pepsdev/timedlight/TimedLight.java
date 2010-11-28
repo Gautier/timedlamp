decc9c6f3bdb45aa7d3d1d2795fb2b229162b46b
Gautier Hayoun <ghayoun@gmail.com>
2010-11-28 20:42:02 +0000

    Initial Commit

---
+++ src/com/pepsdev/timedlight/TimedLight.java
@@ -0,0 +1,15 @@
+package com.pepsdev.timedlight;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimedLight extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
